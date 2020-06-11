3849ba810493530d1eb3de07e9ffa03548f4f4b6
solo95 <wengers1004@naver.com>
2020-06-11 22:43:35 +0900

    solved greedy algorithm

--- solo95/greedy/1439.py
+++ solo95/greedy/1439.py
@@ -1,8 +1,10 @@
 from sys import stdin
 import re
 line = stdin.readline().strip()
-findZero = re.findall('0*', line)
-findOne  = re.findall('1*', line)
-zeroList= list(filter(lambda x:x != '', findZero))
-oneList= list(filter(lambda x:x != '', findOne))
-print(min(len(zeroList), len(oneList)))
+# 정규표현식을 이용하여 0이 연속되는 부분과 1이 연속되는 부분을 추출.
+findZeroGroup = re.findall('0*', line)
+findOneGroup  = re.findall('1*', line)
+# 추출 시 비어있는 '' 값이 생기고 이를 제거하는 과정
+zeroGroupList = list(filter(lambda x:x != '', findZeroGroup))
+oneGroupList  = list(filter(lambda x:x != '', findOneGroup))
+print(min(len(zeroGroupList), len(oneGroupList)))
