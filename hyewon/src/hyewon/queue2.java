452e387926c5c7a969d5d9cdeca8b623b6f83d15
hyewon <hiii456@hanmail.net>
2020-06-16 09:31:35 +0900

    Stack_and_Queue

--- hyewon/src/hyewon/queue2.java
+++ hyewon/src/hyewon/queue2.java
@@ -1,10 +1,48 @@
 package hyewon;
 
-public class queue2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+import java.io.*;
+import java.util.*;
+ 
+public class queue2{
+    public static void main(String[] args) throws IOException {
+    	LinkedList<Integer> que = new LinkedList<Integer>();    // 큐의 생성
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int count = Integer.parseInt(st.nextToken());
+    	
+    	for(int i=0; i<count; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		String temp =st.nextToken();
+    		if(temp.equals("push")) {
+    			que.add(Integer.parseInt(st.nextToken()));
+    		}else if(temp.equals("pop")) {
+    			if(que.size()==0)
+    				bw.write("-1 \n");
+    			else
+    				bw.write(que.poll() + "\n");
+    		}else if(temp.equals("size")) {
+    			bw.write(que.size() + "\n");
+    		}else if(temp.equals("empty")) {
+    			if(que.size()==0)
+    				bw.write("1 \n");
+    			else
+    				bw.write("0 \n");
+    		}else if(temp.equals("front")) {
+    			if(que.size()==0)
+    				bw.write("-1 \n");
+    			else
+    				bw.write(Integer.toString(que.get(0)) + "\n");
+    		}else if(temp.equals("back")) {
+    			if(que.size()==0)
+    				bw.write("-1 \n");
+    			else
+    				bw.write(que.peekLast() + "\n");
+    			}
+    		
+    	}
+    	bw.flush();
+    	bw.close();
+        
+    }
 }
