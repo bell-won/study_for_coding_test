67d5a6958abed32556c113177d90d96e8d3e9c0b
HYEONYY <62040737+hiii456@users.noreply.github.com>
2020-08-27 23:57:37 +0900

    Update cache.java

--- hyewon/src/hyewon/kakaoproblem/cache.java
+++ hyewon/src/hyewon/kakaoproblem/cache.java
@@ -3,19 +3,7 @@ package hyewon.kakaoproblem;
 import java.util.LinkedList;
 
 public class cache {
-	public static void main(String[] args) {
-		System.out.println(solution(3, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork"
-                , "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"})); // 50
-		System.out.println(solution(3, new String[]{"Jeju", "Pangyo", "Seoul", "Jeju"
-                , "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"})); // 21
-        System.out.println(solution(2, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork"
-                , "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"})); // 60
-        System.out.println(solution(5, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork"
-                , "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"})); // 52
-        System.out.println(solution(2, new String[]{"Jeju", "Pangyo", "NewYork", "newyork"})); // 16
-        System.out.println(solution(0, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA"})); // 25
-
-    }
+	
     public static int solution(int cacheSize, String[] cities) {
         int answer = 0;
         
